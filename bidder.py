fb841887144976d6573245ceda405586d5dd9e76
mayhamri <mayhamri8@gmail.com>
2024-10-11 16:38:13 +0300

    competing companies

--- bidder.py
+++ bidder.py
@@ -1,9 +1,12 @@
+from typing import Optional
+
+
 class Bidder:
-    def __init__(self, bid, strategy_func, bidder_company, budget):
+    def __init__(self, bid, strategy_func, bidder_company, budget, company_names: Optional[list] = None):
         self.bid = bid
         self.strategy_func = strategy_func
         self.bidder_company = bidder_company
-        self.company_names = ['Alpha Airlines', 'Beta Resort']
+        self.company_names = ['Alpha Airlines', 'Beta Resort'] if company_names is None else company_names
         self.budget = budget
 
     def get_bid(self, partial_ad):
